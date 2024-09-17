8316e9dd462df098de7428a020e30f52618ca3ff
ZJM0312 <zhang.jinmin@northeastern.edu>
2024-09-17 13:22:48 -0400

    Front-end design
    remember to add back-end design by golang

---
+++ nftbrowser/src/utils.js
@@ -0,0 +1,53 @@
+const origin = "https://deep-index.moralis.io";
+const apiKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub25jZSI6IjFiNjc1MTZiLWRkMTQtNDFkYS1hMDBiLTFkOTk1YmExOTg0MyIsIm9yZ0lkIjoiMzkxNTA0IiwidXNlcklkIjoiNDAyMjgyIiwidHlwZUlkIjoiMTgzZDAyY2ItMzRkMC00MDQyLWFjOTQtMDNlYWM1Y2Y3MmEwIiwidHlwZSI6IlBST0pFQ1QiLCJpYXQiOjE3MTUxOTEyNzgsImV4cCI6NDg3MDk1MTI3OH0.35H56FBCDCpWFTi7OX9WJe1JNqnSHsSGAgHiaj9wduE";
+
+export const getContractNFTs = async (tokenAddress) => {
+    const url = new URL(`${origin}/api/v2/nft/${tokenAddress}`);
+    url.searchParams.append("chain", "eth");
+    url.searchParams.append("format", "decimal");
+    url.searchParams.append("limit", "20");
+
+
+    const response = await fetch(url, {
+        headers: {
+        accept: "application/json",
+        "X-API-KEY": apiKey,
+        },
+    });
+    return response.json();
+};
+  
+  
+export const getContractTrades = async (tokenAddress) => {
+    const url = new URL(`${origin}/api/v2/nft/${tokenAddress}/trades`);
+    url.searchParams.append("chain", "eth");
+    url.searchParams.append("marketplace", "opensea");
+    url.searchParams.append("limit", "20");
+
+
+    const response = await fetch(url, {
+        headers: {
+        accept: "application/json",
+        "X-API-KEY": apiKey,
+        },
+    });
+    return response.json();
+};
+
+
+export const getNFTTransfers = async (tokenAddress, tokenId) => {
+    const url = new URL(
+        `${origin}/api/v2/nft/${tokenAddress}/${tokenId}/transfers`
+    );
+    url.searchParams.append("chain", "eth");
+    url.searchParams.append("format", "decimal");
+    url.searchParams.append("limit", "20");
+    const response = await fetch(url, {
+        headers: {
+            accept: "application/json",
+            "X-API-KEY": apiKey,
+        },
+        });
+        return response.json();
+};
+    
